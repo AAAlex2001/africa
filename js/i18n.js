30fb13c48af553d7edcfc37e0b31fdc58c33fc33
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-06-08 15:58:25 +0300

    правки

--- js/i18n.js
+++ js/i18n.js
@@ -160,7 +160,7 @@ const translations = {
         'footer.link3': 'Политика использования cookie-файлов',
         'footer.copy': '© 2025 Санкт-Петербургский горный университет',
         'dialog.title': 'Форум осуществляется в рамках ',
-        'dialog.titleHighlight': 'второго российско-африканского сырьевого диалога',
+        'dialog.titleHighlight': 'Второго российско-африканского сырьевого диалога',
         'dialog.button': 'Подробнее о сырьевом диалоге',
     },
     en: {
@@ -322,7 +322,7 @@ const translations = {
         'footer.link3': 'Cookie policy',
         'footer.copy': '© 2025 Saint Petersburg Mining University',
         'dialog.title': 'The Forum is conducted as part of the',
-        'dialog.titleHighlight': 'second Russian-African Raw Materials Dialogue',
+        'dialog.titleHighlight': 'Second Russian-African Raw Materials Dialogue',
         'dialog.button': 'Learn more about the raw materials dialogue',
     }
 };
